2c640dc9a6c6dc8312523b42bb3f5be5bae35b7f
Akkras <sarkkanton@gmail.com>
2024-11-01 09:32:32 +0200

    commit ficsa

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function convertTemperature() {
     let temperature = parseFloat(document.getElementById("temperature").value);
     let decimalPlaces = document.querySelector('input[name="decimal"]:checked').value;
     let result;
-    let tempchecker;
+
     
     if ((temperature) < -273.15) {
         document.getElementById("result").innerText = "Syötä kelvollinen lämpötila.";
@@ -16,10 +16,10 @@ function convertTemperature() {
     
     if (conversionType === "c_to_f") {
         result = (temperature *1.8) + 32;
-        tempchecker = fahr
+        
     } else if (conversionType === "f_to_c") {
         result = (temperature - 32) /1.8;
-        tempchecker = celc
+      
     }
     
     document.getElementById("result").innerText = `Tuloksena: ${result.toFixed(decimalPlaces)}°`;
